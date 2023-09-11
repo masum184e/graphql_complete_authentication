90e56370e8d67180990a2e6df8e9374c6e34fc50
Masum Billah <masum184e@gmail.com>
2023-09-11 06:01:41 +0600

    Admin TypeDefs Added

---
+++ types/admin.js
@@ -0,0 +1,21 @@
+import { gql } from "apollo-server-express";
+
+const adminTypeDefs = gql`
+  scalar ID
+
+  type Admin{
+    _id: ID
+    fullName: String!
+    email: String!
+    password: String!
+  }
+
+  type Query{
+    admin(adminId: ID!): Admin
+  }
+
+  type Mutation{
+    adminLogin(email: String!, password: String!): String!
+    removeUser(userId: ID): Boolean!
+  }
+`;
