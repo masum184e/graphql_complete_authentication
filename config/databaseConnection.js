48de3d236f4bebb266f97aca279d1bddef882ea3
Masum Billah <masum184e@gmail.com>
2023-09-11 06:01:54 +0600

    Database Connection Added

---
+++ config/databaseConnection.js
@@ -0,0 +1,23 @@
+import { MongoClient } from 'mongodb';
+
+let database;
+
+const databaseConnection = async (DATABASE_URL, DATABASE_NAME) => {
+  try {
+    const client = new MongoClient(DATABASE_URL);
+    await client.connect();
+    database = client.db(DATABASE_NAME);
+    console.log('Database Connection Successfull');
+  } catch (err) {
+    console.error('Error connecting to MongoDB: ', err);
+  }
+}
+
+const getDatabase = async () => {
+  if(!database){
+    throw new Error('Database not connected');
+  }
+  return database;
+}
+
+export { databaseConnection, getDatabase }
