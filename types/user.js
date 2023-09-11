94276e595c02d98070f682c2a1f77edc01eccb43
Masum Billah <masum184e@gmail.com>
2023-09-11 06:02:07 +0600

    User TypeDefs Added

---
+++ types/user.js
@@ -0,0 +1,32 @@
+import { gql } from "apollo-server-express";
+
+const userTypeDefs = gql`
+  scalar Date
+  scalar ID
+  scalar Upload
+
+  type User{
+    _id: ID!
+    firstName: String!
+    lastName: String!
+    email: String!
+    phoneNumber: String!
+    createdAt: Date!
+    profilePicture: Upload
+  }
+
+  type Query{
+    user(userId: ID!): User!
+    users: [User!]
+  }
+
+  type Mutation{
+    userRegistration(firstName: String!,lastName: String!,email: String!,phoneNumber: String!,password: String!): String!
+    userLogin(email: String!, password: String!): String!
+    userResetPasswordSendEmail(email: String!): Boolean!
+    userResetPassword(userId: ID, token: String!, password: String!): Boolean!
+    userUploadProfilePicture(userId: ID, profilePicture: Upload): Boolean!
+  }
+`;
+
+export default userTypeDefs;
